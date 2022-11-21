43268b8bc32c57c48d9922189205a2874babdc07
Diana <diana.gco@gmail.com>
2022-11-20 19:39:21 -0600

    Adding model and  migrations for encuesta table

--- pozos/encuesta/models.py
+++ pozos/encuesta/models.py
@@ -1,3 +1,50 @@
 from django.db import models
+from django.utils.translation import gettext_lazy as _
 
-# Create your models here.
+class Encuesta(models.Model):
+    MUJER = 'mujer'
+    HOMBRE = 'hombre'
+    
+    SEXO = [
+        (MUJER, _('Mujer')),
+        (HOMBRE, _('Hombre'))
+    ]
+
+    LLAVE =  'llave'
+    GARRAFON = 'garrafon'
+    FILTRO = 'filtro'
+    HERVIDA = 'hervida'
+    OTRA = 'otra'
+
+    AGUAS = [
+        (LLAVE, _('De la llave')),
+        (GARRAFON, _('De garrafón')),
+        (FILTRO, _('De filtro')),
+        (HERVIDA, _('Hervida')),
+        (OTRA, _('Otra'))
+    ]
+
+    SI_NO = (
+        (True, _('Sí')),
+        (False, _('No'))
+    )
+
+    edad = models.PositiveSmallIntegerField()
+    peso = models.PositiveSmallIntegerField()
+    sexo = models.CharField(
+        max_length = 32,
+        choices = SEXO
+    )
+    agua_cocinar = models.CharField(
+        max_length = 32,
+        choices = AGUAS 
+    )
+    agua_tomar = models.CharField(
+        max_length = 32,
+        choices = AGUAS 
+    )
+    cuidador = models.BooleanField(choices = SI_NO) 
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    vasos = models.PositiveSmallIntegerField(default=0)
+
