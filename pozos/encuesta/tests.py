05ea10b8e877fdce6def38006336aab63cfcc85a
Diana <diana.gco@gmail.com>
2023-02-06 22:29:08 -0600

    Adding Encuesta tests

--- pozos/encuesta/tests.py
+++ pozos/encuesta/tests.py
@@ -1,3 +1,9 @@
 from django.test import TestCase
+from .models import Encuesta
 
-# Create your tests here.
+class EncuestaModelTests(TestCase):
+
+    def test_total_vasos_is_0_when_vasos_is_0(self): 
+        encuesta = Encuesta(vasos = 0)
+        self.assertEqual(encuesta.total_vasos, 0)
+
